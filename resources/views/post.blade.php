ba69d54dfe88881810fe73f6a6872c5c99b1aa73
AlyDap <ferdynan.jr@gmail.com>
2024-03-06 22:09:45 +0700

    eps 8 done

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,6 +3,9 @@
 @section('container')
 <article> 
  <h2>{{ $post->title }}</h2>
+
+<p>By. Muhammad Ferdynan ALi Syahbana in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+
  {!! $post->body !!}
 </article>
 <a href="/posts">Back to Post</a>
